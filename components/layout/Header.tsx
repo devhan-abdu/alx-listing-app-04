e860ceed95c85b5604e152d853362edbfaa80ab8
devhan-hub <hanabduman@gmail.com>
2025-07-20 21:31:52 +0300

    initial project setup

---
+++ components/layout/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+    
+    return (
+        <header className = "bg-cyan-300 text-white">
+            <h3 className = " text-xl font-bold">Airbnb</h3>
+            <nav>
+                <ul>
+                    <li>
+                        header
+                        </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
