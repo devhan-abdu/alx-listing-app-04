b512c385a332c27f44999970e2fee58484c349a5
devhan-hub <hanabduman@gmail.com>
2025-08-31 03:47:19 +0300

    task 0

---
+++ components/property/PropertyCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type PropertyCardProps = {
+  key: string;
+  property: any;
+}
+
+const PropertyCard = ({key, property}: PropertyCardProps) => {
+  return (
+    <div>
+       hany
+    </div>
+  )
+}
+
+export default PropertyCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,32 @@
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import PropertyCard from '@/components/properties/PropertyCard';
 
 
 export default function Home() {
-  return (
-   <div className="bg-red-500 min-h-screen">
-    <h1>Hello World</h1>
-    <p>This is a paragraph</p>
-    <button>Click me</button>
-    <input type="text" placeholder="Enter your name" />
-    <select>
-      <option value="1">Option 1</option>
-      <option value="2">Option 2</option>
-    </select>
-    <div className="flex flex-col items-center justify-center">
-      <h1>Hello World</h1>
-      <p>This is a paragraph</p>
-      <button>Click me</button>
-      <input type="text" placeholder="Enter your name" />
-      <select>
-        <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-      </select>
-      <div className="flex flex-col items-center justify-center">
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-      </div>
-       <ul>
-         <li>Item 1</li>
-         <li>Item 2</li>
-         <li>Item 3</li>
-       </ul>
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const response = await axios.get("/api/properties");
+
+        setProperties(response.data);
+      } catch (error) {
+        console.error("Error fetching properties:", error);
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchProperties();
+  }, [])
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {properties.map((property) => (
+        <PropertyCard key={property.id} property={property} />
+      ))}
     </div>
-   </div>
   );
 }
