3db1c02d8b70bd2b98599c62dcacbb555bfa034c
devhan-hub <hanabduman@gmail.com>
2025-08-31 03:58:57 +0300

    task1

---
+++ components/property/PropertyDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PropertyDetail = ({property}: any) => {
+  return (
+    <div>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, id reiciendis, voluptas dicta maiores impedit sapiente blanditiis, quaerat ullam laborum aliquid architecto ut accusantium molestias et in inventore quibusdam? Nesciunt?
+    </div>
+  )
+}
+
+export default PropertyDetail
